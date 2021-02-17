0d633334627dfaca8d3989c2f18ca6386db26de2
bellothornus2 <divanov@cifpfbmoll.eu>
2021-02-17 16:58:52 +0100

    feat(Examen): :sparkles: the base done!

---
+++ damian.examen/src/main/java/edu/ping/damian/examen/develop/App.java
@@ -0,0 +1,13 @@
+package edu.ping.damian.examen.develop;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
