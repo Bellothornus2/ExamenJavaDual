5e5987d50c6e81ae087f9792901b93ab7b7c6641
bellothornus2 <divanov@cifpfbmoll.eu>
2021-02-17 20:29:14 +0100

    feat(Examen): :rocket: all the user stories checked and validated!!!!

--- damian.examen/src/main/java/edu/ping/damian/examen/develop/criteria/MaxBid.java
+++ damian.examen/src/main/java/edu/ping/damian/examen/develop/criteria/MaxBid.java
@@ -2,7 +2,6 @@ package edu.ping.damian.examen.develop.criteria;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
